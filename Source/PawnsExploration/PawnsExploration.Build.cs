61f2b3d43f11b549494f07cf491a169f2cf63170
Eduardo Velez <galeneluna@gmail.com>
2024-01-05 13:28:28 -0600

    Added 2D Pawns and Sprites for animations and created PaperZD Blueprint Characters and added enhanced input movement

--- Source/PawnsExploration/PawnsExploration.Build.cs
+++ Source/PawnsExploration/PawnsExploration.Build.cs
@@ -8,7 +8,7 @@ public class PawnsExploration : ModuleRules
 	{
 		PCHUsage = PCHUsageMode.UseExplicitOrSharedPCHs;
 	
-		PublicDependencyModuleNames.AddRange(new string[] { "Core", "CoreUObject", "Engine", "InputCore", "EnhancedInput" });
+		PublicDependencyModuleNames.AddRange(new string[] { "Core", "CoreUObject", "Engine", "InputCore", "EnhancedInput", "PaperZD" });
 
 		PrivateDependencyModuleNames.AddRange(new string[] {  });
 
